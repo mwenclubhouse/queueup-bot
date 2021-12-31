2ae9b094ac2d3765e12ca583ced9074006c59a27
Matthew Campbell <campb474@purdue.edu>
2021-12-30 23:44:02 -0500

    Not working

--- queuebot/main.ts
+++ queuebot/main.ts
@@ -1,15 +1,15 @@
 import Discord from "discord.js";
-const client = new Discord.Client()
+const client : any = new Discord.Client()
 //const userResponse = require('./common/user-response.js');
 //const {userResponse} = require("./common/user-response.js");
 //require('dotenv').config()
-import {} from 'dotenv/config'
+//import {} from 'dotenv/config';
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
+client.on("message", (msg : any) => {
   if (msg.content === "ping") {
     msg.reply("pong");
   }
@@ -18,15 +18,19 @@ client.on("message", msg => {
 })
 
 
+/*
+async function clear_emojis(message : any) {
+  
+  for (reaction in message.reactions) {
 
-async function clear_emojis(message) {
-    for (reaction in message.reactions) {
-        await reaction.clear();
-    }
+    await reaction.clear();
+  }
 }
+*/
 
 client.login(process.env.TOKEN)
 
-
+/*
 import { userResponse } from "./common/user-response.js";
 const p = new userResponse();
+*/
